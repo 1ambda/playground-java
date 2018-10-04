e5a04d18ae26d387ad31198dda112624211ee81d
1ambda <1amb4a@gmail.com>
2018-10-04 14:31:38 +0900

    test: Add AbstractIntegrationTest

---
+++ spring/src/test/java/integration/authenticate/AuthenticationTest.java
@@ -0,0 +1,13 @@
+package integration.authenticate;
+
+import integration.AbstractIntegrationTest;
+import org.junit.Test;
+import org.springframework.context.annotation.Profile;
+
+public class AuthenticationTest extends AbstractIntegrationTest {
+
+    @Test
+    public void hello() {
+
+    }
+}
