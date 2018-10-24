5347d7cf7c16f350a4922567ebdecc277f92be16
1ambda <1amb4a@gmail.com>
2018-10-24 17:59:14 +0900

    chore(gateway): Add LocalEnvironment class

--- service-gateway/src/main/java/com/github/lambda/gateway/GatewayApplication.java
+++ service-gateway/src/main/java/com/github/lambda/gateway/GatewayApplication.java
@@ -1,22 +1,41 @@
 package com.github.lambda.gateway;
 
+import com.github.lambda.gateway.config.ProfileManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 import javax.annotation.PostConstruct;
+import java.util.Locale;
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class GatewayApplication {
 
-  @PostConstruct
-  void init() {
-    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-  }
+  @Autowired
+  private LocalEnvironment localEnvironment;
 
   public static void main(String[] args) {
     SpringApplication.run(GatewayApplication.class, args);
   }
+
+  @Autowired
+  private ProfileManager profileManager;
+
+  @PostConstruct
+  void after() {
+    // setup Timezone
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+
+    // set local
+    Locale.setDefault(new Locale("en", "US"));
+
+    // configure application
+    if (profileManager.hasLocalProfile()) {
+      localEnvironment.setup();
+    }
+  }
+
 }
---
+++ service-gateway/src/main/java/com/github/lambda/gateway/LocalEnvironment.java
@@ -0,0 +1,54 @@
+package com.github.lambda.gateway;
+
+import com.github.lambda.gateway.domain.user.UserService;
+import com.github.lambda.gateway.domain.user.entity.AuthIdentity;
+import com.github.lambda.gateway.domain.user.entity.User;
+import com.github.lambda.gateway.swagger.model.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile({"local"})
+@Configuration
+public class LocalEnvironment {
+
+  @Autowired
+  private UserService userService;
+
+  @Value("${application.gateway.test.customer-username}")
+  private String testCustomerUsername;
+
+  @Value("${application.gateway.test.customer-password}")
+  private String testCustomerPassword;
+
+  @Value("${application.gateway.test.customer-email}")
+  private String testCustomerEmail;
+
+  public void setup() {
+    addTestCustomer();
+  }
+
+  public void addTestCustomer() {
+    User existing = userService.getUserByUsername(testCustomerUsername);
+
+    if (existing != null) {
+      return;
+    }
+
+    String name = testCustomerUsername.substring(0, 1).toUpperCase() +
+        testCustomerUsername.substring(1);
+
+    UserDTO request = UserDTO.builder()
+        .username(testCustomerUsername)
+        .password(testCustomerPassword)
+        .email(testCustomerUsername + "test@.com")
+        .address("Unknown")
+        .provider(AuthIdentity.Provider.PASSWORD.value())
+        .name(name)
+        .build();
+
+    userService.handleAddNewCustomerRequest(request);
+  }
+
+}
