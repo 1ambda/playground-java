047d6ad4b0f4327e9b2b38a73c26ec2205995296
1ambda <1amb4a@gmail.com>
2018-10-30 12:21:49 +0900

    fix(gateway): Set default, max page size for catalog search

--- service-gateway/src/main/java/com/github/lambda/gateway/domain/catalog/controller/CatalogController.java
+++ service-gateway/src/main/java/com/github/lambda/gateway/domain/catalog/controller/CatalogController.java
@@ -11,7 +11,10 @@ import com.github.lambda.gateway.swagger.server.api.CatalogControllerApi;
 import com.google.common.base.Preconditions;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,6 +32,9 @@ public class CatalogController implements CatalogControllerApi {
 
   private CatalogService catalogService;
   private UserService userService;
+
+  private static final int DefaultPageSize = 8;
+  private static final int MaxPageSize = 48;
 
   @Autowired
   public CatalogController(CatalogService catalogService, UserService userService) {
@@ -80,8 +86,13 @@ public class CatalogController implements CatalogControllerApi {
   @RequestMapping(value = "/catalog/products",
       produces = {"application/json"},
       method = RequestMethod.GET)
-  public ResponseEntity<PaginatedProductDTO> findPaginatedProducts(@ApiIgnore Pageable pageable,
-                                                                   @ApiParam ProductSpecificationRequest query) {
+  public ResponseEntity<PaginatedProductDTO> findPaginatedProducts(
+      @ApiIgnore @PageableDefault(size = DefaultPageSize, direction = Sort.Direction.DESC) Pageable pageable,
+      @ApiParam ProductSpecificationRequest query) {
+
+    if (pageable.getPageSize() > MaxPageSize) {
+      pageable = PageRequest.of(pageable.getPageNumber(), MaxPageSize, pageable.getSort());
+    }
 
     PaginatedProductDTO dto = catalogService.handleGetPaginatedProductsRequest(query, pageable);
 
